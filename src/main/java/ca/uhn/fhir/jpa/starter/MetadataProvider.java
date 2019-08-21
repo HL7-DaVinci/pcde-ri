23846e4a0fc2b9e9e1e345a5953d0006cd989caa
jminieri <jminieri@mitre.org>
2019-08-21 14:36:10 -0400

    Base Reference Implementation for PCDE

---
+++ src/main/java/ca/uhn/fhir/jpa/starter/MetadataProvider.java
@@ -0,0 +1,69 @@
+package ca.uhn.fhir.jpa.starter;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import ca.uhn.fhir.jpa.provider.r4.JpaConformanceProviderR4;
+import org.hl7.fhir.r4.model.*;
+import org.hl7.fhir.r4.model.CapabilityStatement.*;
+import org.hl7.fhir.r4.model.Enumerations.PublicationStatus;
+import ca.uhn.fhir.jpa.dao.DaoConfig;
+import ca.uhn.fhir.jpa.dao.IFhirSystemDao;
+import ca.uhn.fhir.rest.server.RestfulServer;
+import ca.uhn.fhir.rest.api.server.RequestDetails;
+
+public class MetadataProvider extends JpaConformanceProviderR4 {
+  MetadataProvider(RestfulServer theRestfulServer, IFhirSystemDao<Bundle, Meta> theSystemDao, DaoConfig theDaoConfig) {
+    super(theRestfulServer, theSystemDao, theDaoConfig);
+    setCache(false);
+  }
+
+  @Override
+  public CapabilityStatement getServerConformance(HttpServletRequest theRequest) {
+    CapabilityStatement metadata = super.getServerConformance(theRequest);
+    metadata.setTitle("Da Vinci PCDE Reference Implementation");
+    metadata.setStatus(PublicationStatus.DRAFT);
+    metadata.setExperimental(true);
+    metadata.setPublisher("Da Vinci");
+
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(2019, 8, 5, 0, 0, 0);
+    metadata.setDate(calendar.getTime());
+
+    // TODO: add once we have a github repo
+    // CapabilityStatementSoftwareComponent software =
+    //   new CapabilityStatementSoftwareComponent();
+    // software.setName("https://github.com/HL7-DaVinci/drug-formulary");
+    // metadata.setSoftware(software);
+
+    metadata.addImplementationGuide("http://build.fhir.org/ig/HL7/davinci-pcde/index.html");
+    metadata.addImplementationGuide("https://wiki.hl7.org/Da_Vinci_Payer_Coverage_Decision_FHIR_IG_Proposal");
+
+    updateRestComponents(metadata.getRest());
+    return metadata;
+  }
+
+  private void updateRestComponents(
+    List<CapabilityStatementRestComponent> originalRests
+  ) {
+    for(CapabilityStatementRestComponent rest : originalRests) {
+      List<CapabilityStatementRestResourceComponent> resources = rest.getResource();
+      for(CapabilityStatementRestResourceComponent resource : resources) {
+        //List<ResourceInteractionComponent> interactions = new ArrayList<ResourceInteractionComponent>();
+        // interactions.add(new ResourceInteractionComponent().setCode(TypeRestfulInteraction.HISTORYINSTANCE));
+        // interactions.add(new ResourceInteractionComponent().setCode(TypeRestfulInteraction.HISTORYTYPE));
+        // interactions.add(new ResourceInteractionComponent().setCode(TypeRestfulInteraction.READ));
+        // interactions.add(new ResourceInteractionComponent().setCode(TypeRestfulInteraction.SEARCHTYPE));
+        // interactions.add(new ResourceInteractionComponent().setCode(TypeRestfulInteraction.VREAD));
+        // resource.setInteraction(interactions);
+
+        if(resource.getType() == "CarePlan") {
+          resource.setProfile("http://hl7.org/fhir/us/core/StructureDefinition/us-core-careplan");
+        }
+      }
+    }
+  }
+}
